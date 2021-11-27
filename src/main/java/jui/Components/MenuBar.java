8edb51e30ab57b1a4dae1858be4b712d0530f531
Rishon Jonathan R <66567635+RIshonRJ@users.noreply.github.com>
2021-11-27 11:07:35 +0530

    Added Few More Features

--- src/main/java/jui/Components/MenuBar.java
+++ src/main/java/jui/Components/MenuBar.java
@@ -1,4 +1,18 @@
 package jui.Components;
 
+import javax.swing.*;
+
 public class MenuBar {
+    JMenuBar menuBar = new JMenuBar();
+    public void setDifferentMenuBar(MenuBar menuBar) {
+        this.menuBar = menuBar.menuBar;
+    }
+
+    public JMenuBar getMenuBar() {
+        return menuBar;
+    }
+    public void add(MenuItem menuItem)
+    {
+        menuBar.add(menuItem.menuItem);
+    }
 }
