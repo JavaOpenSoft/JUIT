914f287633eaeb64f9f3ea5fb4558e3fd9b5e6d6
Rishon Jonathan R <66567635+RIshonRJ@users.noreply.github.com>
2021-11-27 11:07:35 +0530

    Added Few More Features

---
+++ src/main/java/jui/img/DefaultImages.java
@@ -0,0 +1,11 @@
+package jui.img;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+
+public class DefaultImages {
+    public Image ErrorImage = Toolkit.getDefaultToolkit().getImage("jui/img/error.png");
+    public Image SuccessImage = Toolkit.getDefaultToolkit().getImage("jui/img/success.png");
+    public Image WarningImage = Toolkit.getDefaultToolkit().getImage("jui/img/error.png");
+    public Image AboutImage = Toolkit.getDefaultToolkit().getImage("jui/img/About.png");
+}
