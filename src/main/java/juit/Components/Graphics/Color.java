7ab1d6c1a10db79ba7c1e57a0bbe693f740e71a1
Rishon Jonathan R <66567635+RIshonRJ@users.noreply.github.com>
2021-12-07 22:51:50 +0530

    Added Base Files For 0.6

---
+++ src/main/java/juit/Components/Graphics/Color.java
@@ -0,0 +1,26 @@
+package juit.Components.Graphics;
+
+public class Color {
+    public final int[] Black = {0,0,0};
+    public final int[] White = {256,256,256};
+    public final int[] Red = {256,0,0};
+    public final int[] Lime = {0,256,0};
+    public final int[] Blue = {0,0,256};
+    public final int[] Yellow = {256,256,0};
+    public final int[] Cyan = {0,256,256};
+    public final int[] Magenta = {256,0,256};
+    public final int[] Silver = {192,192,192};
+    public final int[] Gray = {128,128,128};
+    public final int[] Maroon = {0,0,0};
+    public final int[] Olive = {0,0,0};
+    public final int[] Green = {0,0,0};
+    public final int[] Purple = {0,0,0};
+    public final int[] Teal = {0,0,0};
+    public final int[] Navy = {0,0,0};
+    public final int[] Gold = {0,0,0};
+    public final int[] Deep_pink = {0,0,0};
+    public final int[] Pink = {0,0,0};
+
+
+
+}
