fa5aedc973b29b01e13e2cffe72cfe74fef53205
Rishon Jonathan R <66567635+RIshonRJ@users.noreply.github.com>
2021-12-11 23:44:02 +0530

    0.6 Released What's new?: Color class

--- src/main/java/juit/Components/Graphics/Color.java
+++ src/main/java/juit/Components/Graphics/Color.java
@@ -1,6 +1,7 @@
 package juit.Components.Graphics;
 
 public class Color {
+    java.awt.Color color;
     public final int[] Black = {0,0,0};
     public final int[] White = {256,256,256};
     public final int[] Red = {256,0,0};
@@ -11,14 +12,32 @@ public class Color {
     public final int[] Magenta = {256,0,256};
     public final int[] Silver = {192,192,192};
     public final int[] Gray = {128,128,128};
-    public final int[] Maroon = {0,0,0};
-    public final int[] Olive = {0,0,0};
-    public final int[] Green = {0,0,0};
-    public final int[] Purple = {0,0,0};
-    public final int[] Teal = {0,0,0};
-    public final int[] Navy = {0,0,0};
-    public final int[] Gold = {0,0,0};
-    public final int[] Deep_pink = {0,0,0};
-    public final int[] Pink = {0,0,0};
+    public final int[] Maroon = {128,0,0};
+    public final int[] Olive = {128,128,0};
+    public final int[] Green = {0,128,0};
+    public final int[] Purple = {128,0,128};
+    public final int[] Teal = {0,128,128};
+    public final int[] Navy = {0,0,128};
+    public final int[] Gold = {255,215,0};
+    public final int[] Deep_pink = {255,215,0};
+    public final int[] Pink = {255,105,180};
+    public final int[] Orange = {255,165,0};
+    public final int[] HotPink = {255,0, 100};
+    public Color(int r, int g, int b)
+    {
+        this.color = new java.awt.Color(r,g,b);
+    }
+    public int getR(int[] colorIndex)
+    {
+        return colorIndex[0];
+    }
 
+    public int getG(int[] colorIndex)
+    {
+        return colorIndex[1];
+    }
+    public int getB(int[] colorIndex)
+    {
+        return colorIndex[2];
+    }
 }
