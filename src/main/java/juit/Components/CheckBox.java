92f66977a5b72475d33bfaa88e6b6969a5079f07
Rishon Jonathan R <66567635+RIshonRJ@users.noreply.github.com>
2021-12-30 14:24:10 +0530

    Reorganized code, more features added

--- src/main/java/juit/Components/CheckBox.java
+++ src/main/java/juit/Components/CheckBox.java
@@ -5,8 +5,37 @@ import java.awt.event.ActionListener;
 
 public class CheckBox {
     JCheckBox jCheckBox = new JCheckBox();
+    boolean isSelected = false;
+    String Text ="";
     public void addActionListener(ActionListener actionListener)
     {
         jCheckBox.addActionListener(actionListener);
     }
+    public void setSelected(boolean isSelected)
+    {
+        jCheckBox.setSelected(isSelected);
+        this.isSelected = isSelected;
+    }
+    public boolean isSelected()
+    {
+        return isSelected;
+    }
+    public String toString(){
+        String temp = "";
+        if(isSelected == true) temp = "true";
+        if(isSelected == false) temp = "false";
+        return temp;
+    }
+    public void setText(String Text){
+        jCheckBox.setText(Text);
+        this.Text = Text;
+    }
+    public String getTextLocally()
+    {
+        return this.Text;
+    }
+    public String getText(){
+        return jCheckBox.getText();
+    }
+
 }
