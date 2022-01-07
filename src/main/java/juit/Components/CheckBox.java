a50f91b05d77b05d541c91516efc7193c79e361d
Rishon Jonathan R <66567635+RIshonRJ@users.noreply.github.com>
2022-01-07 14:43:36 +0530

    small changes

--- src/main/java/juit/Components/CheckBox.java
+++ src/main/java/juit/Components/CheckBox.java
@@ -5,11 +5,26 @@ import java.awt.event.ActionListener;
 
 public class CheckBox {
     JCheckBox jCheckBox = new JCheckBox();
+    boolean isChecked = false;
+
     public void addActionListener(ActionListener actionListener)
     {
         jCheckBox.addActionListener(actionListener);
     }
-    public void setState(boolean state){
+    public void setState(boolean isChecked){
+        jCheckBox.setSelected(isChecked);
+        this.isChecked = isChecked;
+    }
+    public void setCheckedIcon(String iconPath)
+    {
+
+        jCheckBox.setIcon(new ImageIcon(iconPath));
     }
     public void setSize(int SizeX, int SizeY){}
+    public void setClickable(boolean isClickable){}
+    public void isVisible(boolean bool)
+    {
+        jCheckBox.setVisible(true);
+    }
+    public void setText(String Text){}
 }
---
+++ src/main/java/juit/utils/Image/Icon.java
@@ -0,0 +1,8 @@
+package juit.utils.Image;
+
+public class Icon {
+    javax.swing.Icon icon;
+    public Icon(String iconPath){
+
+    }
+}
