5794497b8f4013c54650def4431cad8fbf1f5c37
Rishon Jonathan R <66567635+RIshonRJ@users.noreply.github.com>
2022-01-08 18:18:57 +0530

    Checkbox is now usable.

--- src/main/java/juit/Components/CheckBox.java
+++ src/main/java/juit/Components/CheckBox.java
@@ -6,6 +6,41 @@ import java.awt.event.ActionListener;
 public class CheckBox {
     JCheckBox jCheckBox = new JCheckBox();
     boolean isChecked = false;
+    private int SizeX;
+    private int SizeY;
+    private int PositionX;
+    private int PositionY;
+
+    public String getText() {
+        return Text;
+    }
+
+    public String getIconPath() {
+        return IconPath;
+    }
+
+    private String Text, IconPath;
+    public boolean getCheckedSate() {
+        return isChecked;
+    }
+
+    public int getSizeX() {
+        return SizeX;
+    }
+
+    public int getSizeY() {
+        return SizeY;
+    }
+
+    public int getPositionX() {
+        return PositionX;
+    }
+
+    public int getPositionY() {
+        return PositionY;
+    }
+
+
 
     public void addActionListener(ActionListener actionListener)
     {
@@ -15,16 +50,35 @@ public class CheckBox {
         jCheckBox.setSelected(isChecked);
         this.isChecked = isChecked;
     }
-    public void setCheckedIcon(String iconPath)
+    public void setCheckedIcon(String IconPath)
     {
-
-        jCheckBox.setIcon(new ImageIcon(iconPath));
+        jCheckBox.setIcon(new ImageIcon(IconPath));
+        this.IconPath = IconPath;
     }
-    public void setSize(int SizeX, int SizeY){}
-    public void setClickable(boolean isClickable){}
+    public void setSize(int SizeX, int SizeY){
+        jCheckBox.setSize(SizeX, SizeY);
+        this.SizeX = SizeX;
+        this.SizeY = SizeY;
+    }
+    public void setPosition(int PositionX, int PositionY){
+        jCheckBox.setBounds(PositionX, PositionY, SizeX, SizeY);
+        this.PositionX = PositionX;
+        this.PositionY = PositionY;
+    }
+    public void setBounds(int PositionX, int PositionY,int SizeX, int SizeY){
+        setSize(SizeX, SizeY);
+        setPosition(PositionX,PositionY);
+
+    }
+    public void setClickable(boolean isClickable){
+
+    }
     public void isVisible(boolean bool)
     {
-        jCheckBox.setVisible(true);
+        jCheckBox.setVisible(bool);
     }
-    public void setText(String Text){}
+    public void setText(String Text){
+        jCheckBox.setText(Text);
+        this.Text = Text;
+    }
 }
