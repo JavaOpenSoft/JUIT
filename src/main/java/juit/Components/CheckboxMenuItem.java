b030daa63e79bef97cafcf2d63a403689a30bb2c
Rishon Jonathan R <66567635+RIshonRJ@users.noreply.github.com>
2022-01-08 19:14:13 +0530

    CheckboxMenuItem.java class is now usable.

--- src/main/java/juit/Components/CheckboxMenuItem.java
+++ src/main/java/juit/Components/CheckboxMenuItem.java
@@ -5,25 +5,38 @@ import java.awt.event.ActionListener;
 
 public class CheckboxMenuItem {
     String Text = "";
+    ActionListener actionListener;
+    boolean isChecked = false;
     JCheckBoxMenuItem jCheckBoxMenuItem = new JCheckBoxMenuItem();
     public void addActionListener(ActionListener actionListener)
     {
         jCheckBoxMenuItem.addActionListener(actionListener);
+        this.actionListener = actionListener;
     }
 
     public JCheckBoxMenuItem getjCheckBoxMenuItem() {
         return jCheckBoxMenuItem;
     }
+
+    public ActionListener getActionListener() {
+        return actionListener;
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
     public void setText(String Text)
     {
         jCheckBoxMenuItem.setText(Text);
+        this.Text = Text;
     }
 
     public String getText() {
         return Text;
     }
-    public void isHighlighted(boolean bool)
-    {
-        jCheckBoxMenuItem.setState(bool);
+    public void setState(boolean isChecked){
+        jCheckBoxMenuItem.setState(isChecked);
+        this.isChecked = isChecked;
     }
 }
