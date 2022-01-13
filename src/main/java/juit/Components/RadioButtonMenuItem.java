96a19011e10937cd13d11b09a8317514a11370ba
Rishon Jonathan R <mithicalaviator85@gmail.com>
2022-01-13 23:32:55 +0530

    Added small changes to the functions in RadioButtonMenuItem.java

--- src/main/java/juit/Components/RadioButtonMenuItem.java
+++ src/main/java/juit/Components/RadioButtonMenuItem.java
@@ -4,15 +4,27 @@ import javax.swing.*;
 
 public class RadioButtonMenuItem {
     JRadioButtonMenuItem jRadioButtonMenuItem = new JRadioButtonMenuItem();
+    boolean isVisible;
     String Text;
     public RadioButtonMenuItem(String Text)
     {
         setText(Text);
     }
+
+    public boolean isVisible() {
+        return isVisible;
+    }
+
     public void setText(String Text)
     {
         jRadioButtonMenuItem.setText(Text);
         this.Text = Text;
     }
-
+    public String getText() {
+        return Text;
+    }
+    public void setHighlighted(boolean isVisible){
+        jRadioButtonMenuItem.setSelected(isVisible);
+        this.isVisible = isVisible;
+    }
 }
