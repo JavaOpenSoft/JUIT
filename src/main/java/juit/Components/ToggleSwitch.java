feceb86966febb8bddc9abb9a09e3f6d62a24c18
Rishon Jonathan R <mithicalaviator85@gmail.com>
2022-01-13 23:32:02 +0530

    made ToggleSwitch.java usable

--- src/main/java/juit/Components/ToggleSwitch.java
+++ src/main/java/juit/Components/ToggleSwitch.java
@@ -4,4 +4,41 @@ import javax.swing.*;
 
 public class ToggleSwitch {
     JToggleButton jToggleButton = new JToggleButton();
+    int SizeX,SizeY,PositionX,PositionY;
+    boolean isVisible, isSwitchActive;
+
+    public int getSizeX() {
+        return SizeX;
+    }
+
+    public int getSizeY() {
+        return SizeY;
+    }
+
+    public int getPositionX() {
+        return PositionX;
+    }
+
+
+    public int getPositionY() {
+        return PositionY;
+    }
+
+
+    public boolean isVisible() {
+        return isVisible;
+    }
+
+    public void setVisible(boolean isVisible) {
+
+        this.isVisible = isVisible;
+    }
+
+    public boolean isSwitchActive() {
+        return isSwitchActive;
+    }
+
+    public void setSwitchActive(boolean isSwitchActive) {
+        this.isSwitchActive = isSwitchActive;
+    }
 }
