637fd32e9b0254ef1374bbebaf6517590e4a244c
Rishon Jonathan R <mithicalaviator85@gmail.com>
2022-01-13 09:47:59 +0530

    Added more functions

--- src/main/java/juit/Components/Separator.java
+++ src/main/java/juit/Components/Separator.java
@@ -4,7 +4,13 @@ import javax.swing.*;
 
 public class Separator {
     JSeparator jSeparator = new JSeparator();
-    int SizeX,SizeY,PositionX,PositionY;
+    private int SizeX, SizeY, PositionX, PositionY;
+    private boolean isVisible;
+    private int orientation;
+
+    public boolean isVisible() {
+        return isVisible;
+    }
 
     public int getSizeX() {
         return SizeX;
@@ -22,18 +28,44 @@ public class Separator {
         return PositionY;
     }
 
-    public void setSize(int SizeX, int SizeY)
-    {
-        jSeparator.setSize(SizeX,SizeY);
+    public void setSize(int SizeX, int SizeY) {
+        jSeparator.setSize(SizeX, SizeY);
         this.SizeX = SizeX;
         this.SizeY = SizeY;
     }
-    public void setPosition(int PositionX,int PositionY)
+
+    public void setPosition(int PositionX, int PositionY) {
+        jSeparator.setBounds(PositionX, PositionY, SizeX, SizeY);
+    }
+
+    public void setPositionAndSize(int PositionX, int PositionY, int SizeX, int SizeY)
     {
-        jSeparator.setBounds(PositionX,PositionY,SizeX,SizeY);
+        jSeparator.setBounds(PositionX, PositionY, SizeX, SizeY);
+        this.PositionX = PositionX;
+        this.PositionY = PositionY;
+        this.SizeX = SizeX;
+        this.SizeY = SizeY;
     }
     public JSeparator getSeperator()
     {
         return jSeparator;
+    }
+    public void setVisible(boolean isVisible){
+        jSeparator.setVisible(isVisible);
+        this.isVisible = isVisible;
+
+    }
+    public int getOrientation()
+    {
+        return jSeparator.getOrientation();
+    }
+    public int getOrientationLocally()
+    {
+        return orientation;
+    }
+    public void setOrientation(int orientation)
+    {
+        jSeparator.setOrientation(orientation);
+        this.orientation = orientation;
     }
 }
