735ad8f572d2fa05b98f7b1d258c6ad2455e5f66
Rishon Jonathan R <mithicalaviator85@gmail.com>
2022-01-15 16:42:18 +0530

    Added few more features to ToggleSwitch.java

--- src/main/java/juit/Components/ToggleSwitch.java
+++ src/main/java/juit/Components/ToggleSwitch.java
@@ -6,6 +6,7 @@ public class ToggleSwitch {
     JToggleButton jToggleButton = new JToggleButton();
     int SizeX,SizeY,PositionX,PositionY;
     boolean isVisible, isSwitchActive;
+    private String Text;
 
     public int getSizeX() {
         return SizeX;
@@ -30,7 +31,7 @@ public class ToggleSwitch {
     }
 
     public void setVisible(boolean isVisible) {
-
+        jToggleButton.setVisible(isVisible);
         this.isVisible = isVisible;
     }
 
@@ -41,4 +42,24 @@ public class ToggleSwitch {
     public void setSwitchActive(boolean isSwitchActive) {
         this.isSwitchActive = isSwitchActive;
     }
+    public void setSize(int SizeX, int SizeY){
+        jToggleButton.setSize(SizeX, SizeY);
+        this.SizeX = SizeX;
+        this.SizeY = SizeY;
+    }
+    public void setPosition(int PositionX, int PositionY)
+    {
+        jToggleButton.setBounds(PositionX,PositionY,SizeX,SizeY);
+        this.PositionX = PositionX;
+        this.PositionY = PositionY;
+    }
+    public void setPositionAndSize(int PositionX, int PositionY,int SizeX, int SizeY){
+        setPosition(PositionX, PositionY);
+        setSize(SizeX,SizeY);
+    }
+    public void setText(String Text){
+        jToggleButton.setText(Text);
+        this.Text = Text;
+    }
+
 }
