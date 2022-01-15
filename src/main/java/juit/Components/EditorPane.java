ba6ed3f96531615d49061f5634aa0a1262834b63
Rishon Jonathan R <mithicalaviator85@gmail.com>
2022-01-15 17:26:14 +0530

    small additions to EditorPane.java

--- src/main/java/juit/Components/EditorPane.java
+++ src/main/java/juit/Components/EditorPane.java
@@ -1,4 +1,55 @@
 package juit.Components;
 
+import javax.swing.*;
+
 public class EditorPane {
+    public int getSizeX() {
+        return SizeX;
+    }
+
+    public int getSizeY() {
+        return SizeY;
+    }
+
+    public int getPositionX() {
+        return PositionX;
+    }
+
+    public int getPositionY() {
+        return PositionY;
+    }
+
+    JEditorPane jEditorPane = new JEditorPane();
+    private int SizeX;
+    private int SizeY;
+    private int PositionX;
+    private int PositionY;
+
+    public void copy()
+    {
+        jEditorPane.copy();
+    }
+    public String getText()
+    {
+        return jEditorPane.getText();
+    }
+    public void setText(String Text){
+        jEditorPane.setText(Text);
+    }
+    public void setSize(int SizeX, int SizeY){
+        jEditorPane.setSize(SizeX, SizeY);
+        this.SizeX = SizeX;
+        this.SizeY = SizeY;
+    }
+    public void setPosition(int PositionX, int PositionY){
+        jEditorPane.setBounds(PositionX, PositionY,SizeX,SizeY);
+        this.PositionX = PositionX;
+        this.PositionY = PositionY;
+    }
+    public void setPositionAndSize(int PositionX, int PositionY, int SizeX, int SizeY)
+    {
+        setPosition(PositionX,PositionY);
+        setSize(SizeX,SizeY);
+    }
+
 }
