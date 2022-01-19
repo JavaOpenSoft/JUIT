3ee589b12de66f2bd7be3526e759509eabd5e6fe
Rishon Jonathan R <mithicalaviator85@gmail.com>
2022-01-19 23:24:39 +0530

    addedsmalll changes

--- src/main/java/juit/Components/TabPane.java
+++ src/main/java/juit/Components/TabPane.java
@@ -1,4 +1,10 @@
 package juit.Components;
 
+import javax.swing.*;
+
 public class TabPane {
+    javax.swing.JTabbedPane jTabbedPane = new JTabbedPane();
+    public void setSize(int SizeX, int SizeY){
+
+    }
 }
