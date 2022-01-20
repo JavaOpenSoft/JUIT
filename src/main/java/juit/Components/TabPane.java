56fb066e0deec58c9f7c9ce0e78df6407fdb880c
Rishon Jonathan R <mithicalaviator85@gmail.com>
2022-01-20 11:54:16 +0530

    last ever release of 0.9 and the 0. versions

--- src/main/java/juit/Components/TabPane.java
+++ src/main/java/juit/Components/TabPane.java
@@ -1,6 +1,7 @@
 package juit.Components;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TabPane {
     javax.swing.JTabbedPane jTabbedPane = new JTabbedPane();
@@ -39,5 +40,70 @@ public class TabPane {
     public void setPositionAndSize(int PositionX, int PositionY,int SizeX, int SizeY){
         setPosition(PositionX,PositionY);
         setSize(SizeX, SizeY);
+    }
+    public void add(Button button){
+        jTabbedPane.add(button.jButton);
+    }
+    public void add(CheckBox checkBox){
+        jTabbedPane.add(checkBox.jCheckBox);
+    }
+    public void add(EditorPane editorPane){
+        jTabbedPane.add(editorPane.jEditorPane);
+    }
+    public void add(FileChooser fileChooser){
+        jTabbedPane.add(fileChooser.jFileChooser);
+    }
+    public void add(ImageFrame imageFrame){
+        jTabbedPane.add(imageFrame.imageFrame);
+    }
+    public void add(Label label){
+        jTabbedPane.add(label.label);
+    }
+    public void add(Panel panel){
+        jTabbedPane.add(panel.jPanel);
+    }
+    public void add(PasswordField passwordField){
+        jTabbedPane.add(passwordField.jPasswordField);
+    }
+    public void add(ProgressBar progressBar){
+        jTabbedPane.add(progressBar.progressBar);
+    }
+    public void add(RadioButton radioButton){
+        jTabbedPane.add(radioButton.jRadioButton);
+    }
+    public void add(ScrollBar scrollBar, String Type){
+        if(Type.equals("HORIZONTAL"))jTabbedPane.add(scrollBar.BottomScrollBar);
+        if(Type.equals("VERTICAL"))jTabbedPane.add(scrollBar.jScrollBar);
+        if(Type.equals("HORIZONTAL AND VERTICAL")){
+            jTabbedPane.add(scrollBar.BottomScrollBar);
+            jTabbedPane.add(scrollBar.jScrollBar);
+        }
+    }
+    public void add(ScrollPane scrollPane){
+        jTabbedPane.add(scrollPane.jScrollPane);
+    }
+    public void add(Separator separator){
+        jTabbedPane.add(separator.jSeparator);
+    }
+    public void add(Slider slider){
+        jTabbedPane.add(slider.jSlider);
+    }
+    public void add(Spinner spinner){
+        jTabbedPane.add(spinner.jSpinner);
+    }
+    public void add(TabPane tabPane){
+        jTabbedPane.add(tabPane.jTabbedPane);
+    }
+    public void add(TextArea textArea){
+        jTabbedPane.add(textArea.jTextArea);
+    }
+    public void add(TextField textField){
+        jTabbedPane.add(textField.jTextField);
+    }
+    public void add(TextPane textPane){
+        jTabbedPane.add(textPane.textPane);
+    }
+    public void add(ToggleSwitch toggleSwitch){
+        jTabbedPane.add(toggleSwitch.jToggleButton);
     }
 }
