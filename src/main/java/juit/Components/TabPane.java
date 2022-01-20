48992f56ec203eaae8ba94497d506262fc8e0f71
Rishon Jonathan R <mithicalaviator85@gmail.com>
2022-01-20 11:22:14 +0530

    added small changes

--- src/main/java/juit/Components/TabPane.java
+++ src/main/java/juit/Components/TabPane.java
@@ -4,7 +4,40 @@ import javax.swing.*;
 
 public class TabPane {
     javax.swing.JTabbedPane jTabbedPane = new JTabbedPane();
-    public void setSize(int SizeX, int SizeY){
+    private int SizeX;
 
+    public int getSizeX() {
+        return SizeX;
+    }
+
+    public int getSizeY() {
+        return SizeY;
+    }
+
+    public int getPositionX() {
+        return PositionX;
+    }
+
+    public int getPositionY() {
+        return PositionY;
+    }
+
+    private int SizeY;
+    private int PositionX;
+    private int PositionY;
+
+    public void setSize(int SizeX, int SizeY){
+        jTabbedPane.setSize(SizeX, SizeY);
+        this.SizeX = SizeX;
+        this.SizeY = SizeY;
+    }
+    public void setPosition(int PositionX, int PositionY){
+        jTabbedPane.setBounds(PositionX, PositionY,SizeX,SizeY);
+        this.PositionX = PositionX;
+        this.PositionY = PositionY;
+    }
+    public void setPositionAndSize(int PositionX, int PositionY,int SizeX, int SizeY){
+        setPosition(PositionX,PositionY);
+        setSize(SizeX, SizeY);
     }
 }
