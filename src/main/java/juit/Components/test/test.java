3b07aaebe8b6d68360a73419f32278a4ec377931
Rishon Jonathan R <mithicalaviator85@gmail.com>
2022-02-11 17:52:50 +0530

    added refreshed binaries

--- src/main/java/juit/Components/test/test.java
+++ src/main/java/juit/Components/test/test.java
@@ -1,10 +1,19 @@
 package juit.Components.test;
 
 import juit.Components.Button;
+import juit.Components.Window;
+
+import javax.swing.*;
 
 public class test {
     public static void main(String[] args){
+        Window window = new Window();
         Button button = new Button();
+        JPanel panel = new JPanel();
+        window.setSize(1000,1000);
+        button.setVisible(true);
+        window.add(button);
+        window.setVisible(true);
 
     }
 }
